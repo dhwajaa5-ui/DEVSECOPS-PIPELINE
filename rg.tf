7c4ca126728e2831489c66bcb921828a6f01fa0e
Dhwaja <dhwajaa5@gmail.com>
2026-05-27 00:56:06 +0530

    new rg added

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg0001" {
   name="rg0001-dhwaja"
   location = "central india"
 }
+resource "azurerm_resource_group" "rg0002" {
+  name="rg0002-dhwaja"
+  location = "central us"
+}
